4fa9720fb1c693a8f89c43a2b409ce667be319ba
Kunal-Rout <fusion5kunal@gmail.com>
2025-07-22 10:47:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,19 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-nltk.download('punkt')
-nltk.download('stopwords')
+# Add this block right after imports
+try:
+    # Try to download NLTK data
+    nltk.download('punkt', quiet=True)
+    nltk.download('stopwords', quiet=True)
+except Exception as e:
+    # If download fails, try setting a custom download path
+    import os
+    nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
+    os.makedirs(nltk_data_dir, exist_ok=True)
+    nltk.data.path.append(nltk_data_dir)
+    nltk.download('punkt', download_dir=nltk_data_dir, quiet=True)
+    nltk.download('stopwords', download_dir=nltk_data_dir, quiet=True)
 
 ps = PorterStemmer()
 
